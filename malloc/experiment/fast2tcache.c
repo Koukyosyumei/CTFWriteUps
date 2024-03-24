07c5d8da45344e3fe967d757d6d11b892e9f9d85
Koukyosyumei <koukyosyumei@hotmail.com>
2024-03-24 10:28:05 +0900

    update

--- malloc/experiment/fast2tcache.c
+++ malloc/experiment/fast2tcache.c
@@ -4,8 +4,11 @@ int main(void) {
     void *p[4];
 
     for (int i = 0; i < sizeof(p) / sizeof(void *); i++) p[i] = malloc(0x18);
+
     for (int i = 0; i < 7; i++) free(calloc(1, 0x18));
+
     for (int i = 0; i < sizeof(p) / sizeof(void *); i++) free(p[i]);
+
     for (int i = 0; i < 2; i++) malloc(0x18);
 
     calloc(1, 0x18);
